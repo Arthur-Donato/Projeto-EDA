0b32a6ffab6cbb9ff1dfb5d518d4255a6170e782
Arthur Barbosa <arthurdonatobarbosa110@gmail.com>
2025-04-04 13:37:37 -0300

    Implementando o QuickSort

--- atividade_eda/src/main/java/br/com/atividade/example/Main.java
+++ atividade_eda/src/main/java/br/com/atividade/example/Main.java
@@ -5,7 +5,19 @@ import br.com.atividade.example.Ordenacao.Ordenacao;
 public class Main {
     public static void main(String[] args) {
         Ordenacao ordenacao = new Ordenacao();
-        int[] array = {2,1,4,5};
-        System.out.println(ordenacao.bubbleSort(array));
+        int[] array = {2,1,5,4};
+
+        long retornoDeFuncao = ordenacao.random_quickSort(array);
+
+        if(retornoDeFuncao == 0){
+            System.out.println("O vetor ja estava ordenado");
+        }
+        else if(retornoDeFuncao == 1){
+            System.out.println("O vetor foi ordenado com sucesso!");
+        }
+
+        for(int i : array){
+            System.out.println(i);
+        }
     }
 }
--- atividade_eda/src/main/java/br/com/atividade/example/Ordenacao/Ordenacao.java
+++ atividade_eda/src/main/java/br/com/atividade/example/Ordenacao/Ordenacao.java
@@ -1,5 +1,5 @@
 package br.com.atividade.example.Ordenacao;
-
+import java.util.Random;
 public class Ordenacao implements Ordenacao_IF{
 
     @Override
@@ -36,6 +36,7 @@ public class Ordenacao implements Ordenacao_IF{
 
     @Override
     public long selectionSort(int[] numeros) {
+        
         return -1;
     }
 
@@ -52,26 +53,141 @@ public class Ordenacao implements Ordenacao_IF{
 
     @Override
     public long quickSort(int[] numeros) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'quickSort'");
+        if(checaVetorOrdenado(numeros)){
+            return 0;
+        }
+        else{
+            quickSortPrimeiroElemento(numeros, 0, numeros.length - 1);
+            return 1;
+        }
     }
+
+    public void quickSortPrimeiroElemento(int[] numeros, int primeiroIndice, int ultimoIndice){
+        if(primeiroIndice < ultimoIndice){
+            int pivot = particaoPivotPrimeiroElemento(numeros, primeiroIndice, ultimoIndice);
+            quickSortPrimeiroElemento(numeros, primeiroIndice, pivot - 1);
+            quickSortPrimeiroElemento(numeros, pivot + 1, ultimoIndice);
+        }
+    }
+
+    public int particaoPivotPrimeiroElemento(int[] numeros, int primeiroIndice, int ultimoIndice){
+        int pivot = numeros[primeiroIndice];
+        int i = primeiroIndice + 1;
+        int j = ultimoIndice;
+
+        while(i <= j){
+            if(numeros[i] <= pivot){
+                i++;
+            }
+            else if(numeros[j] > pivot){
+                j--;
+            }
+            else{
+                trocarElementos(numeros, i, j);
+            }
+        }
+        trocarElementos(numeros, primeiroIndice, j);
+        return j;
+    }
+
 
     @Override
     public long random_quickSort(int[] numeros) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'random_quickSort'");
+        if(checaVetorOrdenado(numeros)){
+            return 0;
+        }
+        else{
+            quickSortElementoRandom(numeros, 0, numeros.length - 1);
+            return 1;
+        }
+    }
+
+    public void quickSortElementoRandom(int[] numeros, int primeiroIndice, int ultimoIndice){
+        if(primeiroIndice < ultimoIndice){
+            int pivot = particaoPivotElementoAleatorio(numeros, primeiroIndice, ultimoIndice);
+            quickSortElementoRandom(numeros, primeiroIndice, pivot - 1);
+            quickSortElementoRandom(numeros, pivot + 1, ultimoIndice);
+        }
+    }
+
+    public int particaoPivotElementoAleatorio(int[] numeros, int primeiroIndice, int ultimoIndice){
+        Random random = new Random();
+        int indiceAleatorio = random.nextInt(primeiroIndice, ultimoIndice);
+        
+        trocarElementos(numeros, indiceAleatorio, ultimoIndice);
+        int pivot = numeros[ultimoIndice];
+        int i = primeiroIndice - 1;
+        
+        for(int j = primeiroIndice; j < ultimoIndice; j++){
+            if(numeros[j] < pivot){
+                i++;
+                trocarElementos(numeros, i, j);
+            }
+        }
+        trocarElementos(numeros, i + 1, ultimoIndice);
+        return i + 1;
     }
 
     @Override
     public long quickSort_Java(int[] numeros) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'quickSort_Java'");
+        if(checaVetorOrdenado(numeros)){
+            return 0;
+        }
+        else{
+            quickSortJava(numeros, 0, numeros.length - 1);
+            return 1;
+        }
     }
+
+    public void quickSortJava(int[] numeros, int primeiroIndice, int ultimoIndice){
+        if(primeiroIndice < ultimoIndice){
+            if(numeros[primeiroIndice] > numeros[ultimoIndice]){
+                trocarElementos(numeros, primeiroIndice, ultimoIndice);
+            }
+
+            int primeiroPivot = numeros[primeiroIndice];
+            int segundoPivot = numeros[ultimoIndice];
+
+            int i = primeiroIndice + 1;
+            int j = ultimoIndice - 1;
+            int k = i;
+
+            while(k <= j){
+                if(numeros[i] < primeiroPivot){
+                    trocarElementos(numeros, k,i);
+                    i++;
+                }
+                else if(numeros[i] > segundoPivot){
+                    trocarElementos(numeros, k, j);
+                    j--;
+                    k--;
+                }
+                k++;
+            }
+            i--;
+            j++;
+
+            trocarElementos(numeros, primeiroIndice, i);
+            trocarElementos(numeros, ultimoIndice, j);
+
+            quickSortJava(numeros, primeiroIndice, i - 1);
+            quickSortJava(numeros, i + 1, j - 1);
+            quickSortJava(numeros, j + 1, ultimoIndice);
+        }
+    }
+
+    
 
     @Override
     public long countingSort(int[] numeros) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'countingSort'");
+    }
+
+    public void trocarElementos(int[] numeros, int primeiroIndice, int segundoIndice){
+        int numeroTemporario = numeros[primeiroIndice];
+        numeros[primeiroIndice] = numeros[segundoIndice];
+        numeros[segundoIndice] = numeroTemporario;
     }
     
 }
